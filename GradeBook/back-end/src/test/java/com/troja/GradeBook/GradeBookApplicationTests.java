84ec3932309c21862d5f3d285deccf764f33debc
Piotr Trojan <trojanpiotrek@interia.pl>
2024-07-19 07:57:58 +0200

    added back-end part of project

---
+++ GradeBook/back-end/src/test/java/com/troja/GradeBook/GradeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.troja.GradeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
